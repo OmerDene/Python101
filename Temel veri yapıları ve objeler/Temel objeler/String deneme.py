559969a748ad8740a83f0f59cf18c7b0bc97d509
OmerDene <omerdene@gmail.com>
2023-01-21 21:28:37 +0300

    sayi okuma

--- Temel veri yapıları ve objeler/Temel objeler/String deneme.py
+++ Temel veri yapıları ve objeler/Temel objeler/String deneme.py
@@ -1,4 +1,4 @@
-mys = {"omer","mehmet","sss"}
+"""mys = {"omer","mehmet","sss"}
 print(type(mys))
 mys.remove("omer")
 mys.add("huseyin")
@@ -12,8 +12,22 @@ print(sozluk.items())
 a = "17"
 print(len(a))
 b = 19
-
 print(type(b))
+"""
+
+
+
+
+
+
+def toplama(*parametreler): # Artık parametreler değişkenini bir demet gibi kullanabilirim.
+    toplam =  0
+    for i in parametreler:
+        toplam += i
+    return toplam
+print(toplama(3,444,44,4,4,4,4,4,4,4,4,4))
+   
+
 
 
 
