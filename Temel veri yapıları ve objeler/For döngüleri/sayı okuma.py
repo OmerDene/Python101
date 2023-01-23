47d792dad32a07288cd49402fadb6010a02b2140
OmerDene <omerdene@gmail.com>
2023-01-23 23:51:18 +0300

    mukkemmel sayilar

--- Temel veri yapıları ve objeler/For döngüleri/sayı okuma.py
+++ Temel veri yapıları ve objeler/For döngüleri/sayı okuma.py
@@ -12,3 +12,17 @@ def okunus(sayı):
 sayı = int(input("Sayı:"))
 
 print(okunus(sayı))
+
+sayı = int(input("Sayı:"))
+
+i = 1
+toplam = 0
+while (i < sayı):
+    if (sayı % i == 0):
+        toplam += i
+    i += 1
+
+if (toplam == sayı):
+    print(sayı,"mükemmel bir sayıdır.")
+else:
+    print(sayı,"mükemmel bir sayı değildir.")
