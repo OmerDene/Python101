85a0e85c29e2b4846f82ecb210f801c14e834d6d
OmerDene <omerdene@gmail.com>
2023-01-27 20:10:41 +0300

    kim milyoner update

--- Temel veri yapıları ve objeler/Temel objeler/Kim milyoner olmak ister.py
+++ Temel veri yapıları ve objeler/Temel objeler/Kim milyoner olmak ister.py
@@ -1,22 +1,24 @@
+import random
+liste = ["Kevser Hanım","Murat Bey","Özkan Bey","Ömer Bey"]
+secim = random.choice(liste)
 while True:
     print("KİM MİLYONER OLMAK İSTER YARIŞMASI ")
-    Secenek1 = "A"
-    Secenek2 = "B"
-    Secenek3 = "C"
-    Secenek4 = "D"
+    print("Yarışmacımız {} geliyor.. Alkış lütfen..." .format(secim))
+    Secenek1 = "a"
+    Secenek2 = "b"
+    Secenek3 = "c"
+    Secenek4 = "d"
     evet = "evet"
     hayır = "hayır"
     jokerhakkı = 1
     print("Türkiyenin başkenti neresidir  ?","A : Konya", "B : Adıyaman" , "C : Ankara","D : İstanbul", sep ="\n" )
     cevap2 = input("Joker kullanmak ister misiniz : ")
-    if (cevap2 == evet):
+    if (cevap2 == "evet"):
         jokerhakkı -= 1
         print("Seyirciye soruluyor...", "Cevap A : %27", "Cevap B : %15", "Cevap C : %52", "Cevap D : %6", sep="\n")
-    elif (cevap2 == hayır):
+    elif (cevap2 == "hayır"):
         print("Cevap veriliyor.. ")
-    else:
-        print("Geçerli bir ifade giriniz")
-        continue
+
     cevap1 = input("Şıkkı seçiniz :")
 
     if(cevap1 == Secenek1 ):
@@ -27,49 +29,50 @@ while True:
         break
     elif(cevap1 == Secenek3):
         print("Tebrikler!")
-        break
+
     elif(cevap1 == Secenek4):
         print("Elendiniz")
         break
-    else:
-        break
-while True:
-        print("Avrupanın en büyük toprak ölçüsüne sahip ülkesi hangisidir ? ", "A : Rusya", "B : Almanya",
+
+
+    print("Avrupanın en büyük toprak ölçüsüne sahip ülkesi hangisidir ? ", "A : Rusya", "B : Almanya",
               "C : Hırvatistan", "D : Belçika", sep="\n")
-        cevap2 = input("Joker kullanmak ister misiniz : ")
-        if (cevap2 == evet and jokerhakkı !=0):
+    cevap2 = input("Joker kullanmak ister misiniz : ")
+    if (cevap2 == "evet" and jokerhakkı !=0):
             print("Seyirciye soruluyor...", "Cevap A : %52", "Cevap B : %27", "Cevap C : %15", "Cevap D : %6", sep="\n")
-        elif(jokerhakkı==0):
+            jokerhakkı -= 1
+    elif (cevap2 == "hayır"):
+            print("Cevap veriliyor.. ")
+    elif(jokerhakkı==0 and cevap2=="evet"):
             print("Seyirci jokeriniz kalmamıştır...")
 
-        elif (cevap2 == hayır):
-            print("Cevap veriliyor.. ")
-        cevap1 = input("Şıkkı seçiniz :")
-        if (cevap1 == Secenek1):
+
+    cevap1 = input("Şıkkı seçiniz :")
+    if (cevap1 == Secenek1):
             print("Tebrikler!")
-            break
-        elif (cevap1 == Secenek2):
+
+    elif (cevap1 == Secenek2):
             print("Elendiniz")
             break
-        elif (cevap1 == Secenek3):
+    elif (cevap1 == Secenek3):
             print("Elendiniz!")
             break
-        elif (cevap1 == Secenek4):
+    elif (cevap1 == Secenek4):
             print("Elendiniz")
             break
-        else:
-            break
-while True:
+
+
     print("Dünyanın uydusu hangisidir ? ", "A : Ay", "B : jupiter",
           "C : Başakşehir", "D : Satürn", sep="\n")
     cevap2 = input("Joker kullanmak ister misiniz : ")
-    if (cevap2 == evet and jokerhakkı != 0):
+    if (cevap2 == "evet" and jokerhakkı != 0):
         print("Seyirciye soruluyor...", "Cevap A : %52", "Cevap B : %27", "Cevap C : %15", "Cevap D : %6", sep="\n")
+    elif (cevap2 == "hayır"):
+        print("Cevap veriliyor.. ")
     elif (jokerhakkı == 0):
         print("Seyirci jokeriniz kalmamıştır...")
 
-    elif (cevap2 == hayır):
-        print("Cevap veriliyor.. ")
+
     cevap1 = input("Şıkkı seçiniz :")
     if (cevap1 == Secenek1):
         print("Tebrikler!,Büyük Ödülü Kazandınız...")
@@ -86,57 +89,3 @@ while True:
     else:
         break
         print("Yarışma sonlanmıştır")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""art became sufficiently developed to be embodied
-in sonatas. From the beginning of the 17th century until 
-late in the 18th, the cantata for one or two solo voices 
-with accompaniment of basso continuo (and perhaps a few 
-solo instruments) was a principal form of Italian vocal 
-chamber music.A cantata consisted first of a declamatory 
-narrative or scene in recitative, held together by a 
-primitive aria repeated at intervals. Fine examples may 
-be found in the church music of Giacomo Carissimi; and 
-the English vocal solos of Henry Purcell (such as Mad 
-Tom and Mad Bess) show the utmost that can be made of 
-this archaic form. With the rise of the da capo aria, 
-the cantata became a group of two or three arias joined
-by recitative. George Frideric Handel's numerous Italian' \
-'duets and trios are examples on a rather large' \
-scale. His Latin motet Silete Venti, for soprano solo, shows the use of this form in church music"""
-
-
-
-
-
-
