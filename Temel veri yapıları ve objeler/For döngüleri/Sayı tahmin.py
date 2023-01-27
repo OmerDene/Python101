d438be1cddd24eaabb1aa4e7c553257de651cea1
OmerDene <omerdene@gmail.com>
2023-01-27 18:54:26 +0300

    guess what update

--- Temel veri yapıları ve objeler/For döngüleri/Sayı tahmin.py
+++ Temel veri yapıları ve objeler/For döngüleri/Sayı tahmin.py
@@ -1,9 +1,52 @@
-toplam = 0
-while True:
-    sayı = input("sayı giriniz")
-    if (sayı == "q"):
-        break
-    sayı = int(sayı)
+import random
+import time
+print("guess what... ")
 
-    toplam += sayı
-print("sayınız",toplam)
+sayımız = random.randint(1,2)
+tahmin_hakkı = 7
+while True:
+    tahmin_sayısı = int(input("Tahmininizi giriniz: "))
+    if(tahmin_sayısı<sayımız):
+        print("Bilgiler kontrol ediliyor...")
+        time.sleep(1)
+        print("Sayımız tahmininizden yüksektir...")
+        tahmin_hakkı -= 1
+    elif(tahmin_sayısı>sayımız):
+        print("Bilgiler kontrol ediliyor...")
+        time.sleep(1)
+        print("Sayımız tahmininizden düşüktür...")
+        tahmin_hakkı -= 1
+    elif(sayımız==tahmin_sayısı):
+        print("Bilgiler kontrol ediliyor...")
+        time.sleep(1)
+        print("Tebrikler! ",sayımız)
+        yeniden = input("Yeniden oynamak ister misiniz: ")
+        if(yeniden=="evet" or yeniden == "EVET" or yeniden=="yes" or yeniden=="YES"):
+            print("Eglence Devam Ediyor...")
+            continue
+        elif(yeniden=="hayır" or yeniden== "HAYIR" or yeniden=="no" or yeniden=="NO"):
+            print("Yine Bekleriz...")
+            break
+        else:
+            print("Geçersiz tuşlama yaptınız, çıkış yapılıyor...")
+            break
+
+
+    if (tahmin_hakkı == 0):
+        print("Tahmin hakkınız dolmuştur...")
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Temel veri yapıları ve objeler/For döngüleri/maas hesapla.py
+++ Temel veri yapıları ve objeler/For döngüleri/maas hesapla.py
@@ -15,5 +15,20 @@ for i in salaries:
         y = int(i*20/100 + i)
         new_salaries.append(y)
         print(y)
+"""
+Alternatif hesaplama 
 
+
+maaslar = [1000, 2000, 3000, 4000, 5000]
+
+def zam_yap(maas):
+    if maas >= 3000:  # Eğer maaş 3000'den büyük ve eşitse
+        return (maas * 10 / 100) + maas  # %10 zam yap
+    else:  # Değilse
+        return (maas * 20 / 100) + maas  # %20 zam yap
+
+
+for maas in maaslar:  # Maaşlar üzerine döngü uygula
+    print(zam_yap(maas))  # zamlı maaşı ekrana yazdır
+"""
 
