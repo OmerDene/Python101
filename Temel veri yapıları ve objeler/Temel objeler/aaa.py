69da97e1cc201125fd3dc45cb09bda9e37b3623a
OmerDene <omerdene@gmail.com>
2023-01-31 13:30:37 +0300

    kumanda tam

--- Temel veri yapıları ve objeler/Temel objeler/aaa.py
+++ Temel veri yapıları ve objeler/Temel objeler/aaa.py
@@ -1,14 +1,5 @@
-import random
 
-a =["Elazığ","Adıyaman","Sivas","Ankara","Edirne","Yozgat", "İstanbul","Agrı","İzmir","Antalya",]
 
-rastgele = random.choice(a)
-
-for i in rastgele:
-    if(i == "Ankara"):
-        print("Buldunuz!")
-
-print(rastgele)
 
 
 
--- Temel veri yapıları ve objeler/nesne/kumanda.py
+++ Temel veri yapıları ve objeler/nesne/kumanda.py
@@ -49,3 +49,88 @@ class Kumanda():
             else:
                 print("Ses Güncellendi:",self.tv_ses)
                 break
+    def kanal_ekle(self,kanal_ismi):
+
+        print("Kanal ekleniyor....")
+        time.sleep(1)
+
+        self.kanal_listesi.append(kanal_ismi)
+
+        print("Kanal Eklendi.....")
+    def rastgele_kanal(self):
+
+        rastgele = random.randint(0,len(self.kanal_listesi)-1)
+
+
+        self.kanal = self.kanal_listesi[rastgele]
+
+        print("Şu anki Kanal:" ,self.kanal)
+    def __len__(self):
+
+        return len(self.kanal_listesi)
+
+    def __str__(self):
+
+        return "Tv Durumu: {}\nTv Ses: {}\nKanal Listesi: {}\nŞu anki kanal: {}\n".format(self.tv_durum,self.tv_ses,self.kanal_listesi,self.kanal)
+
+
+kumanda = Kumanda()
+
+
+print("""
+
+Televizyon Uygulaması
+
+
+1. Tv Aç
+
+2. Tv Kapat
+
+3. Ses Ayarları
+
+4. Kanal Ekle
+
+5. Kanal Sayısını Öğrenme
+
+6. Rastgele Kanala Geçme
+
+7. Televizyon Bilgileri
+
+Çıkmak için 'q' ya basın.
+""")
+
+
+while True:
+
+    işlem = input("İşlemi Seçiniz:")
+
+    if (işlem == "q"):
+        print("Program Sonlandırılıyor...")
+        break
+
+    elif (işlem == "1"):
+        kumanda.tv_ac()
+    elif (işlem == "2"):
+        kumanda.tv_kapat()
+
+    elif (işlem == "3"):
+        kumanda.ses_ayarları()
+
+    elif (işlem == "4"):
+        kanal_isimleri = input("Kanal isimlerini ',' ile ayırarak girin:")
+
+        kanal_listesi = kanal_isimleri.split(",")
+
+        for eklenecekler in kanal_listesi:
+            kumanda.kanal_ekle(eklenecekler)
+    elif (işlem == "5"):
+
+        print("Kanal Sayısı:",len(kumanda))
+
+    elif (işlem == "6"):
+        kumanda.rastgele_kanal()
+    elif (işlem == "7"):
+        print(kumanda)
+
+    else:
+        print("Geçersiz İşlem......")
