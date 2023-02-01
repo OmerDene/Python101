9241e0959fa9203cf4c23678cc298e8f1e3680e0
OmerDene <omerdene@gmail.com>
2023-02-01 10:10:15 +0300

    sayi harf ayir

--- Temel veri yapıları ve objeler/Temel objeler/aaa.py
+++ Temel veri yapıları ve objeler/Temel objeler/aaa.py
@@ -1,3 +1,17 @@
+sayı = []
+harf = []
+
+a = input("Metin ya da sayı giriniz: ")
+
+for i in a:
+    if i.isalpha():
+        harf.append(i)
+
+    elif i.isnumeric():
+        sayı.append(i)
+
+print("Sayılar:", sayı,"\nHarfler:", harf)
+
 
 
 
