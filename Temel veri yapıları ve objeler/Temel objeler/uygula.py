ba045326504e6c10dbd1a4f327b083ffd2bab1c1
OmerDene <omerdene@gmail.com>
2023-02-08 11:37:06 +0300

    pandas uygulama

--- Temel veri yapıları ve objeler/Temel objeler/uygula.py
+++ Temel veri yapıları ve objeler/Temel objeler/uygula.py
@@ -1,11 +1,25 @@
-def üçgen_mi(demet):
-    if (abs(demet[0] + demet[1]) > demet[2] and abs(demet[0] + demet[2]) > demet[1] and abs(demet[1] + demet[2]) >
-            demet[0]):
-        return True
-    else:
-        return False
+import pandas as pd
 
+#a = pd.Series([10,88,3,4,5])
+#print(a.axes)
+#print(a.tail(1))
 
-liste = [(3, 4, 5), (6, 8, 10), (3, 10, 7)]
+#a = pd.Series([99,22,332,94,5], index = [0,1,2,3,4])
+#a[2] = 1
+#print(a[1:3])
 
-print(list(filter(üçgen_mi, liste)))
+#sozluk = {"reg":10, "log":11, "cart": 12}
+#print(sozluk.keys())
+#c= df = pd.Series(sozluk)
+#print(df)
+#a = pd.concat([c,c,c]) # iki ve daha fazla seriyi birlestirme
+#print(a)
+
+seri = pd.Series([1,3,5,6,7 ],index= ["a","b","c","d","e"])
+#print(seri)
+
+#print(seri["b"])
+#print(seri[["a","b"]])
+
+#print(seri["a":"c"])
+
