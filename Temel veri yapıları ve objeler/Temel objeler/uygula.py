bebb73695b49ac55956c0942b192f295000ea0b5
OmerDene <omerdene@gmail.com>
2023-02-08 22:16:01 +0300

    up

--- Temel veri yapıları ve objeler/Temel objeler/uygula.py
+++ Temel veri yapıları ve objeler/Temel objeler/uygula.py
@@ -41,11 +41,41 @@ import numpy as np
 #c =df.rename(columns={"merhaba":"a","yeni":"b"}) # istedigimiz degiskenin ya da kolonun ismini güncelleme
 #print(c)
 
-dizi = np.array([1,2,3,4,5,6,7,8,9]).reshape(3,3)
+#dizi = np.array([1,2,3,4,5,6,7,8,9]).reshape(3,3)
 #print(dizi)
-df = pd.DataFrame(dizi,columns=["omer","mehmet","masa"],index=[0,2,4])
+#df = pd.DataFrame(dizi,columns=["omer","mehmet","masa"],index=[0,2,4])
 #print(df)
-print(df.axes) # dataframe in genel bilgileri
+#print(df.axes) # dataframe in genel bilgileri
+dizi = np.arange(9).reshape(3,3)
+#print(dizi)
+df = pd.DataFrame(dizi,columns=["ilk kolon","ikinci kolon","ucuncu kolon"])
+#a =df.drop(1,axis=0)
+#print(a)
+#print(df)
+#df.drop(1, axis = 0, inplace = True) # bu sekilde inplace parametresi ile yazınca 1. satır komple gider ve yeni df degiskeni buna gore cagrılır
+#print(df)
+#liste = [0,1]
+#a =df.drop(liste,axis=0)
+#print(a)
+#print(df)
+#print(df[["ilk kolon","ikinci kolon"]])
+#df["dördüncü kolon"] = df["ilk kolon"] * df["ikinci kolon"] # ekstra degisken ya da kolon bu sekilde eklenir.
+#print(df)
 
+#a =df.drop("dördüncü kolon",axis=1) # degisken ya da kolon silme(gecici)
+#print(a)
+#df.drop("dördüncü kolon", axis = 1, inplace = True) # bu sekilde kolon kalıcı gider(kalıcı)
+#print(df)
+
+m = np.random.randint(1,30, size = (10,3))
+df = pd.DataFrame(m, columns = ["var1","var2","var3"])
+
+#liste = ["var2","var1"]
+
+#print(df["var2"][0:5])
+#print(df[0:5][liste])
+
+
+#print(df[df.var1>15][["var1","var2","var3"]])
 
 
